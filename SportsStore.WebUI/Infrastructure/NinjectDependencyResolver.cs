9c61b044fbba1df84c3b26f2d10371e2692509db
patryk.kubacki@outlook.com <patryk.kubacki@outlook.com>
2017-02-26 22:20:59 +0100

    ADD: Configuration container DI

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+
+	public class NinjectDependencyResolver : IDependencyResolver
+	{
+		IKernel _kernel;
+
+		public NinjectDependencyResolver(IKernel kernel)
+		{
+			_kernel = kernel;
+			AddBindings();
+		}
+
+		public object GetService(Type serviceType)
+		{
+			return _kernel.TryGet(serviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return _kernel.GetAll(serviceType);
+		}
+
+		void AddBindings()
+		{
+			
+		}
+	}
+
+}
