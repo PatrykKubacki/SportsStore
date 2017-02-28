e9dd89f87040841c331bf24bfe05251e5fa50cf1
patryk.kubacki@outlook.com <patryk.kubacki@outlook.com>
2017-02-28 18:49:40 +0100

    NEW: Add paggination and unit tests

---
+++ SportsStore.UnitTests/PaginationTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.HtmlHelpers;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.UnitTests
+{
+
+	[TestClass]
+	public class PaginationTests
+	{
+		[TestMethod]
+		public void CanPaginate()
+		{
+			var mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new List<Product>
+			{
+				new Product { ProductID = 1, Name = "P1" },
+				new Product { ProductID = 2, Name = "P2" },
+				new Product { ProductID = 3, Name = "P3" },
+				new Product { ProductID = 4, Name = "P4" },
+				new Product { ProductID = 5, Name = "P5" },
+			});
+			var controller = new ProductController(mock.Object) { PageSize = 3 };
+
+			var result = (ProductListViewModel)controller.List(2).Model;
+
+			Product[] products = result.Products.ToArray();
+			Assert.IsTrue(products.Length == 2);
+			Assert.IsTrue(products[0].Name == "P4");
+			Assert.IsTrue(products[1].Name == "P5");
+		}
+
+		[TestMethod]
+		public void CanGeneratePageLinks()
+		{
+			HtmlHelper helper = null;
+			var pagingInfo = new PagingInfo { TotalItems = 28, CurrentPage = 2, ItemsPerPage = 10 };
+			Func<int, string> pageUrlDelegate = i => $"Strona{i}";
+
+			MvcHtmlString result = helper.PageLinks(pagingInfo, pageUrlDelegate);
+
+			Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>"
++ @"<a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
++ @"<a class=""btn btn-default"" href=""Strona3"">3</a>",result.ToString());
+
+		}
+
+		[TestMethod]
+		public void CanSendPaginationViewModel() {
+			var mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new List<Product>
+			{
+				new Product { ProductID = 1, Name = "P1" },
+				new Product { ProductID = 2, Name = "P2" },
+				new Product { ProductID = 3, Name = "P3" },
+				new Product { ProductID = 4, Name = "P4" },
+				new Product { ProductID = 5, Name = "P5" },
+			});
+			var controller = new ProductController(mock.Object) { PageSize = 3 };
+			var result = (ProductListViewModel)controller.List(2).Model;
+
+			PagingInfo pageinfo = result.PagingInfo;
+			Assert.AreEqual(pageinfo.CurrentPage,2);
+			Assert.AreEqual(pageinfo.ItemsPerPage,3);
+			Assert.AreEqual(pageinfo.TotalItems,5);
+			Assert.AreEqual(pageinfo.TotalPages,2);
+		}
+	}
+
+}
--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,24 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
-    public class ProductController : Controller
-    {
-	    IProductRepository _repository;
 
-	    public ProductController(IProductRepository repository)
-	    {
-		    _repository = repository;
-	    }
+	public class ProductController : Controller
+	{
+		IProductRepository _repository;
+		public int PageSize = 4;
 
-        public ActionResult List()
-        {
-            return View(_repository.Products);
-        }
-    }
+		public ProductController(IProductRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public ViewResult List(int page = 1)
+		{
+			var model = new ProductListViewModel
+			{
+				Products = _repository.Products.OrderBy(p => p.ProductID)
+										       .Skip((page - 1) * PageSize)
+											   .Take(PageSize),
+				PagingInfo = new PagingInfo
+				{
+					CurrentPage = page,
+					ItemsPerPage = PageSize,
+					TotalItems = _repository.Products.Count()
+				}
+			};
+			return View(model);
+		}
+	}
+
 }
